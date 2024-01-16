70fdce33a079fb17d2a687810fec0e25ac08563a
Zer0-hex <Zer0.hex.orz@gmail.com>
2024-01-16 20:54:15 +0800

    5

--- roles/configure-base-env/files/nvim/lua/options.lua
+++ roles/configure-base-env/files/nvim/lua/options.lua
@@ -2,12 +2,13 @@
 vim.opt.clipboard = 'unnamedplus' -- use system clipboard
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
+vim.opt.listchars = { space = '_', tab = '--->', eol = '↵'}
 
 -- Tab
 vim.opt.tabstop = 4 -- number of visual spaces per TAB
 vim.opt.softtabstop = 4 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
-vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+vim.opt.expandtab = false -- tabs are spaces, mainly because of python
 
 -- UI config
 vim.opt.number = true -- show absolute number
